1d5d89a3e07eba826a96bdb1cc4d7cfea279a92e
itsDavidev <davidlez2mgk@gmail.com>
2023-01-21 17:02:36 -0600

    🧪 remove logs in kata valid parenthesses

--- katas/validParentheses.ts
+++ katas/validParentheses.ts
@@ -17,10 +17,8 @@ export function isValid(s: string): boolean {
   let res = s;
   for (let i = 0; i < s.length; i++) {
     res = res.replaceAll('()', '').replaceAll('[]', '').replaceAll('{}', '');
-    console.log(res);
   }
 
-  console.log(res.replace('()', ''));
   if (res.length === 0) return true;
   return false;
 }
