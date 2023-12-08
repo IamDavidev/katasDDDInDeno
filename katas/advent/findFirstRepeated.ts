4ec503a91d0b38e1fd66a9c93186c2163db19202
IamDavidev <davidlez2mgk@gmail.com>
2023-12-08 16:37:11 -0600

    Refactor findFirstRepeated function to use a map for efficient duplicate checking

--- katas/advent/findFirstRepeated.ts
+++ katas/advent/findFirstRepeated.ts
@@ -6,13 +6,29 @@ type Gifts = number[]
  * @returns
  */
 export function findFirstRepeated(gifts: Gifts): number {
+	/**
+	 * Map of gifts to boolean values
+	 */
 	const giftsMap = new Map<number, boolean>()
 
+	/**
+	 * Iterate through the gifts array
+	 */
 	for (const gift of gifts) {
+		/**
+		 * If the gift is already in the map, return it
+		 * as the first repeated gift
+		 */
 		if (giftsMap.has(gift)) return gift
 
+		/**
+		 * Otherwise, add the gift to the map
+		 */
 		giftsMap.set(gift, true)
 	}
 
+	/**
+	 * If no repeated gifts are found, return -1
+	 */
 	return -1
 }
