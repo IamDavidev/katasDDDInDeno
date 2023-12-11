34dae021baec62ff7400673d4eb346521cce2984
David Lezama <85713808+IamDavidev@users.noreply.github.com>
2023-12-11 23:48:52 +0000

    Refactor simulateSleighMovement function

--- katas/advent/simulate-sleigh-movement.ts
+++ katas/advent/simulate-sleigh-movement.ts
@@ -7,7 +7,7 @@ Conditions
 	* = Barrera abierta = Open barrier
 	| = Barrera cerrada = Closed barrier
 
-	After 5 time units, all closed barriers are opened.
+	After 5 time units, all closed barriers are opened. 
 */
 
 /**
@@ -20,9 +20,57 @@ export function simulateSleighMovement(
 	road: string,
 	time: number
 ): Array<string> {
-	const result = new Array<string>()
+	/**
+	 * The result is a set of roads
+	 */
+	const result: string[] = []
+	/**
+	 * Mantain the raw road
+	 */
+	let rawSplitRoad = road.split('')
 
-	return result
+
+
+	/**
+	 * Open all barriers in the road
+	 */
+	const openAllBarriers = (): void => {
+		rawSplitRoad = rawSplitRoad.join('').replaceAll('|', '*').split('')
+	}
+
+	/**
+	 * Function to push a new state of the road
+	 * @param index  index of the road to push
+	 */
+	const pushNewStateRoad = (index: number): void => {
+		const santaChar = 'S'
+		const roadChar = '.'
+		const rawRoad = [...rawSplitRoad]
+
+		rawRoad[0] = roadChar
+		rawRoad[index] = santaChar
+
+		result.push(rawRoad.join(''))
+	}
+
+	/**
+	 * Passed time
+	 */
+	let passedTime = 0
+	let index = 0  
+	while (passedTime < time) {
+		const step = rawSplitRoad[index]
+
+		if (step === '|') {
+			pushNewStateRoad(index - 1)
+		} else {
+			pushNewStateRoad(index)
+			index++
+		}
+
+		if (passedTime === 4) openAllBarriers()
+
+		passedTime++
+	}
+	return [...result]
 }
-
-
