7c51ebf667e61693f16ac26361a8f3c2001a3325
David Lezama <85713808+IamDavidev@users.noreply.github.com>
2023-12-12 13:52:56 +0000

    Add function to organize gifts

---
+++ katas/advent/organize-gifts.ts
@@ -0,0 +1,18 @@
+/**
+ * 
+ * Function that takes a string of gifts and organizes them into containers
+ * 
+ * e.g `76a11b` --> [a]{a}{a}(aaaaaa){b}(b)
+ * 
+ * The containers are:
+ * 	- pallets: 50 gifts - [a]
+ * 	- boxes: 10 gifts - {a}{a}
+ * 	- bags: 1 gift - (aaaaaa)
+ *
+ * @param str - The string of gifts
+ * @returns - The organized gifts
+ */
+export function organizeGifts(str: string): string {
+	return str;
+}
+
