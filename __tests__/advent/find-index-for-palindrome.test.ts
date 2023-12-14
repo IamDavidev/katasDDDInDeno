184bf6081f444556dffd3db798db46f00fe0ed5b
David Lezama <85713808+IamDavidev@users.noreply.github.com>
2023-12-14 23:24:44 +0000

    Add tests for findIndexsForPalindrome function

---
+++ __tests__/advent/find-index-for-palindrome.test.ts
@@ -0,0 +1,132 @@
+import { assertEquals } from '@testing/asserts.ts'
+import { findIndexsForPalindrome } from '../../katas/advent/find-indexs-for-palidrome.ts'
+import { describe, it } from '@testing/bdd.ts'
+
+function assertIndexForPalindrome(
+	input: string,
+	expected: [number, number] | [] | null
+) {
+	/**
+	 * @When
+	 */
+	const indexs = findIndexsForPalindrome(input)
+
+	/**
+	 * @Then
+	 */
+	assertEquals(indexs, expected)
+}
+
+describe('Tests for Find indexs for palindrome kata', () => {
+	it('Should return [] when the words are already a palindrome', () => {
+		/**
+		 * @Given
+		 */
+		const input = [
+			'anna',
+			'civic',
+			'kayak',
+			'level',
+			'madam',
+			'mom',
+			'noon',
+			'racecar',
+			'radar',
+			'redder',
+			'refer',
+			'repaper',
+			'rotator',
+			'rotor',
+			'sagas',
+			'solos',
+			'stats',
+			'tenet',
+			'wow',
+		]
+		const expected = [] as []
+
+		/**
+		 * @Run
+		 */
+		for (const word of input) {
+			assertIndexForPalindrome(word, expected)
+		}
+	})
+
+	it('Should return [0,1] when the word is "abab"', () => {
+		/**
+		 * @Given
+		 */
+		const input = 'abab'
+		const expected = [0, 1] as [number, number]
+
+		/**
+		 * @Then
+		 */
+		assertIndexForPalindrome(input, expected)
+	})
+
+	it('should return [1,3] when the word is aaababa', () => {
+		/**
+		 * @Given
+		 */
+		const input = 'aaababa'
+		const expected = [1, 3] as [number, number]
+
+		assertIndexForPalindrome(input, expected)
+	})
+
+	it('should return [4,8] when the word is rotaratov', () => {
+		/**
+		 * @Given
+		 */
+		const input = 'rotaratov'
+		const expected = [4, 8] as [number, number]
+
+		/**
+		 * @Run
+		 */
+		assertIndexForPalindrome(input, expected)
+	})
+
+	it('should return null when is not possible to make a palindrome', () => {
+		/**
+		 * @Given
+		 */
+
+		const inputs = [
+			'abc',
+			'abcd',
+			'abcde',
+			'abcdef',
+			'abcdefg',
+			'abcdefgh',
+			'abcdefghi',
+			'abcdefghij',
+			'abcdefghijk',
+			'abcdefghijkl',
+			'abcdefghijklm',
+			'abcdefghijklmn',
+			'abcdefghijklmno',
+			'abcdefghijklmnop',
+			'abcdefghijklmnopq',
+			'abcdefghijklmnopqr',
+			'abcdefghijklmnopqrs',
+			'abcdefghijklmnopqrst',
+			'abcdefghijklmnopqrstu',
+			'abcdefghijklmnopqrstuv',
+			'abcdefghijklmnopqrstuvw',
+			'abcdefghijklmnopqrstuvwx',
+			'abcdefghijklmnopqrstuvwxy',
+			'abcdefghijklmnopqrstuvwxyz',
+		]
+		const expected = null
+
+		/**
+		 * @Run
+		 */
+		for (const input of inputs) {
+			assertIndexForPalindrome(input, expected)
+		}
+	})
+})
