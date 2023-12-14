b646a18b26a478337b9968656bb3fb45f0d3a6e0
David Lezama <85713808+IamDavidev@users.noreply.github.com>
2023-12-14 01:39:35 +0000

    Add tests for createChristmasTree function

---
+++ __tests__/advent/create-christmas-tree.test.ts
@@ -0,0 +1,47 @@
+import { describe, it } from '@testing/bdd.ts'
+import { createChristmasTree } from '../../katas/advent/create-christmas-tree.ts'
+import { assertEquals } from '@testing/asserts.ts'
+
+function checkTreeCreation(symbols: string, height: number,  expected: string) {
+    /**
+     * @When
+     */
+    const tree = createChristmasTree(symbols, height)
+
+    /**
+     * @Then
+     */
+    assertEquals(tree, expected)
+}
+
+describe("Create Christmas Tree", () => {
+
+    it("should return a tree with height 1", () => {
+        /**
+         * @Given
+         */
+        const symbols = '*'
+        const height = 1
+        const expected = '*\n|\n'
+
+        /**
+         * @Run
+         */
+        checkTreeCreation(symbols, height, expected)
+    })
+
+    it("should return a tree with height 3", () =>{
+        /**
+         * @Given
+         */
+        const symbols = '*'
+        const height = 3
+        const expectedTree = '  *\n * *\n* * *\n  |\n'
+
+       /**
+        * @Run
+        */
+       checkTreeCreation(symbols, height, expectedTree)
+    })
+
+})
