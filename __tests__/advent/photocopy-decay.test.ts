33fa8919a693dbb7eff59c312bb47a288da0ebcd
David Lezama <85713808+IamDavidev@users.noreply.github.com>
2023-12-15 15:59:02 +0000

    Refactor photocopy-decay.test.ts

--- __tests__/advent/photocopy-decay.test.ts
+++ __tests__/advent/photocopy-decay.test.ts
@@ -14,8 +14,13 @@ function assertIsCopy(original: string, copy: string, expected: boolean) {
 	assertEquals(isCopy, expected)
 }
 
-describe('Tests for Photocopy decay verifier kata', () => {
-	it('Should return true when the copy is the same as the original', () => {
+// describe('Tests for Photocopy decay verifier kata', () => {
+type Original = string
+type Copy = string
+
+Deno.test(
+	'Should return true when the copy is the same as the original',
+	() => {
 		/**
 		 * @Given
 		 */
@@ -26,5 +31,70 @@ describe('Tests for Photocopy decay verifier kata', () => {
 		 * @Then
 		 */
 		assertIsCopy(original, copy, true)
-	})
+	}
+)
+
+Deno.test(
+	"Should return false when the copy is not the same as the original because it's the same level of decay",
+	() => {
+		/**
+		 * @Given
+		 */
+		const cards = new Map<Original, Copy>([
+			['H', 'W'],
+			['e', 'o'],
+			['L', 'R'],
+		])
+
+		const expected = false
+
+		/**
+		 * @Run
+		 */
+
+		for (const [original, copy] of cards) {
+			assertIsCopy(original, copy, expected)
+		}
+	}
+)
+
+Deno.test(
+	'Should return false when the copy has different length than the original',
+	() => {
+		/**
+		 * @Given
+		 */
+		const original = 'Santa Claus is coming'
+		const copy = 'sa#ta Cl#us i+'
+
+		const expected = false
+
+		/**
+		 * @Run
+		 */
+		assertIsCopy(original, copy, expected)
+	}
+)
+
+Deno.test('Should return true when the copy is only one level of decay', () => {
+	/**
+	 * @Given
+	 */
+	const cards = new Map<Original, Copy>([
+		['A', 'a'],
+		['Z', 'z'],
+		['a', '#'],
+		['z', '+'],
+		['a', ':'],
+		['z', '.'],
+		['A', ' '],
+	])
+
+	/**
+	 * @Run
+	 */
+
+	for (const [original, copy] of cards) {
+		assertIsCopy(original, copy, true)
+	}
 })
