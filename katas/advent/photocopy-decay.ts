4ca82aa0fc2f77720afd3ea55c9f15a30e0a3ca3
David Lezama <85713808+IamDavidev@users.noreply.github.com>
2023-12-16 06:40:06 +0000

    Refactor photocopyDecayVerifier function

--- katas/advent/photocopy-decay.ts
+++ katas/advent/photocopy-decay.ts
@@ -1,16 +1,55 @@
 /**
- * 
+ *
  * Function that checks if the text has been copied correctly.
- * 
+ *
  * Degradation rules:
  * A-Z -> a-z -> # -> + -> : -> . -> " "
- * 
+ *
  * @param original  The original text
  * @param copy  The text has been copied
- * @returns 
+ * @returns
  */
-export function photocopyDecayVerifier(original: string, copy: string): boolean {
-	const isCopy = copy === original
+export function photocopyDecayVerifier(
+	original: string,
+	copy: string
+): boolean {
+	console.log({
+		original,
+		copy,
+	})
 
-	return isCopy
+	if (original.length !== copy.length) return false
+	const isCopy = original === copy || original.toLowerCase() === copy
+
+	if (isCopy) return true
+
+	const charsMayus = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+	const charsMinus = 'abcdefghijklmnopqrstuvwxyz'
+	const charsSpecial = '#+:. '
+
+	const verifierIsCopy = (original: string, copy: string): boolean => {
+		let validCharsToDecay = original
+
+		if (charsMayus.includes(original)) {
+			validCharsToDecay += original.toLowerCase() + charsSpecial
+		}
+		if (charsMinus.includes(original)) {
+			validCharsToDecay += charsSpecial
+		}
+
+		if (charsSpecial.includes(original)) {
+			validCharsToDecay = charsSpecial.slice(charsSpecial.indexOf(original))
+		}
+
+		console.log({ original, copy, validCharsToDecay })
+
+		return validCharsToDecay.includes(copy)
+	}
+
+	for (let iterator = 0; iterator < original.length; iterator++) {
+		const isCopyChar = verifierIsCopy(original[iterator], copy[iterator])
+		if (!isCopyChar) return false
+	}
+
+	return true
 }
