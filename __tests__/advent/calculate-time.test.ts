e1baa2eae1cb9a5616f7a54ecf998d2e1f33fc9a
David Lezama <85713808+IamDavidev@users.noreply.github.com>
2023-12-17 06:18:18 +0000

    Add calculateTime test cases

---
+++ __tests__/advent/calculate-time.test.ts
@@ -0,0 +1,55 @@
+import { assertEquals } from '@testing/asserts.ts'
+import {
+	calculateTime,
+	type Deliveries,
+} from '../../katas/advent/calculate-time.ts'
+
+export function checkRestTime(deliveries: Deliveries, expectedTime: string) {
+	/**
+	 * @When
+	 */
+	const currentTime = calculateTime(deliveries)
+
+	/**
+	 * @Then
+	 */
+	assertEquals(currentTime, expectedTime)
+}
+
+Deno.test('calculateTime', async t => {
+	const it = t.step
+
+	await it('should return -07:00:00 when the deliveries are empty', () => {
+		/**
+		 * @Given
+		 */
+		const deliveries: Deliveries = []
+		const expectedTime = '07:00:00'
+
+		/**
+		 * @Run
+		 */
+		checkRestTime(deliveries, expectedTime)
+	})
+
+	await it('should return -06:00:00 when the all deliveries sum 1 hour', () => {
+		/**
+		 * @Given
+		 */
+		const deliveries: Deliveries[] = [
+			['00:15:00', '00:15:00', '00:15:00', '00:15:00'],
+			['01:00:00'],
+			['00:00:00', '01:00:00'],
+			['00:00:00', '00:30:00', '00:30:00'],
+		]
+		const expectedTime = '06:00:00'
+
+		/**
+		 * @Run
+		 */
+
+		for (const delivery of deliveries) {
+			checkRestTime(delivery, expectedTime)
+		}
+	})
+})
