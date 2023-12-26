3d5eff9ac31e81ee3694c0226a1086f09c90989d
David Lezama <85713808+IamDavidev@users.noreply.github.com>
2023-12-26 04:31:14 +0000

    Refactor findBalancedSegment function to improve performance

--- katas/advent/find-balanced-segment.ts
+++ katas/advent/find-balanced-segment.ts
@@ -7,21 +7,66 @@ export type Message = Array<Segment>
  * @param message Array of 1s and 0s that represents a message
  * @returns  [start, end] of the longest balanced segment
  */
-export function findBalancedSegment(message: Message): [number, number] {
-  console.table(message)
+export function findBalancedSegment(message: Message): [number, number] | [] {
+  let longestSegment = [0, 0] as [number, number]
+  let zeros = 0
+  let ones = 0
+  let max = 0
 
-  return [0, 0]
+  const actions = {
+    0: () => zeros++,
+    1: () => ones++,
+  }
+
+  for (let outerIndex = 0; outerIndex < message.length; outerIndex++) {
+    zeros = 0
+    ones = 0
+    for (
+      let innerIndex = outerIndex;
+      innerIndex < message.length;
+      innerIndex++
+    ) {
+      const innerSegment = message[innerIndex]
+
+      actions[innerSegment]()
+
+      const isNewLongestSegment = zeros === ones && zeros > max
+
+      if (isNewLongestSegment) {
+        longestSegment = [outerIndex, innerIndex]
+        max = zeros
+      }
+    }
+  }
+  if (longestSegment[0] === 0 && longestSegment[1] === 0) return []
+  return longestSegment
 }
 
-findBalancedSegment([1, 1, 0, 1, 1, 0, 1, 1])
+console.log(findBalancedSegment([1, 1, 0, 1, 1, 0, 1, 1]))
 //                         |________|
 // posición del segmento:    [2, 5]
 // más largo equilibrado
 // de 0s y 1s
 
-findBalancedSegment([1, 1, 0])
+console.log(findBalancedSegment([1, 1, 0]))
 //                      |__|
 //                     [1, 2]
 
-findBalancedSegment([1, 1, 1])
+console.log(findBalancedSegment([1, 1, 1]))
 // no hay segmentos equilibrados: []
+
+console.log(findBalancedSegment([0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1]))
+/*
+
+Test: 
+
+Expected:
+[
+  5,
+  10
+]
+
+Actual:
+[]
+
+*/
