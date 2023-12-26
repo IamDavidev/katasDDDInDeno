d8c170943b0355ff003e37cc97cfed929483bc11
David Lezama <85713808+IamDavidev@users.noreply.github.com>
2023-12-26 16:54:42 +0000

    Refactor tests for findBalancedSegment function

--- __tests__/advent/find-balanced-segment.test.ts
+++ __tests__/advent/find-balanced-segment.test.ts
@@ -4,7 +4,7 @@ import {
   findBalancedSegment,
 } from '../../katas/advent/find-balanced-segment.ts'
 
-function assertBalancedSegment(msg: Message, seg: [number, number]) {
+function assertBalancedSegment(msg: Message, seg: [number, number] | []) {
   /**
    * @When
    */
@@ -16,6 +16,100 @@ function assertBalancedSegment(msg: Message, seg: [number, number]) {
   assertEquals(longestBalancedSegment, seg)
 }
 
-// Deno.test('findBalancedSegment([1, 1, 0, 1, 1, 0, 1, 1])', () => {
-//   assertBalancedSegment([1, 1, 0, 1, 1, 0, 1, 1], [2, 5])
-// })
+Deno.test('Properties of longest balanced segment', async t => {
+  const commonMessage: Message = [1, 1, 0, 1, 1, 0, 1, 1]
+  const it = t.step
+
+  await it('Should return array of length 2', () => {
+    /**
+     * @Given
+     */
+    const expectedLength = 2
+
+    /**
+     * @When
+     */
+    const longestBalancedSegment = findBalancedSegment(commonMessage)
+
+    /**
+     * @Then
+     */
+    assertEquals(longestBalancedSegment.length, expectedLength)
+  })
+
+  await it('Should return array of numbers', () => {
+    /**
+     * @Given
+     */
+    const expectedType = 'number'
+
+    /**
+     * @When
+     */
+    const longestBalancedSegment = findBalancedSegment(commonMessage)
+
+    /**
+     * @Then
+     */
+
+    for (const index of longestBalancedSegment) {
+      assertEquals(typeof index, expectedType)
+    }
+  })
+})
+
+Deno.test('Balaced Segment', async t => {
+  const it = t.step
+
+  await it('should return [2,5] when the message is [1, 1, 0, 1, 1, 0, 1, 1]', () => {
+    /**
+     * @Given
+     */
+    const message: Message = [1, 1, 0, 1, 1, 0, 1, 1]
+    const expectedSegment: [number, number] = [2, 5]
+
+    /**
+     * @Assert
+     */
+    assertBalancedSegment(message, expectedSegment)
+  })
+
+  await it('should return [1,2] when the message is [1, 1, 0]', () => {
+    /**
+     * @Given
+     */
+    const message: Message = [1, 1, 0]
+    const expectedSegment: [number, number] = [1, 2]
+
+    /**
+     * @Assert
+     */
+    assertBalancedSegment(message, expectedSegment)
+  })
+
+  await it('should return [] when the message is [1, 1, 1]', () => {
+    /**
+     * @Given
+     */
+    const message: Message = [1, 1, 1]
+    const expectedSegment: [] = []
+
+    /**
+     * @Assert
+     */
+    assertBalancedSegment(message, expectedSegment)
+  })
+
+  await it('should return [5,10] when the message is [0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0]', () => {
+    /**
+     * @Given
+     */
+    const message: Message = [0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0]
+    const expectedSegment: [number, number] = [5, 10]
+
+    /**
+     * @Assert
+     */
+    assertBalancedSegment(message, expectedSegment)
+  })
+})
