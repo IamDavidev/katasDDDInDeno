1be92c3dd0753003e368ca4d2be96b9fb8295d5a
David Lezama <85713808+IamDavidev@users.noreply.github.com>
2023-12-27 22:22:55 +0000

    Refactor compiler function to use a dictionary for actions

--- katas/advent/compiler.ts
+++ katas/advent/compiler.ts
@@ -12,6 +12,35 @@ export type InputCode = string
  */
 export function compile(code: InputCode): number {
   let counter = 0
-  console.log('CODE >> ', code)
-  return 0
+  const instructions = code.split('')
+
+  let flagIndex = null as number | null
+  let currentIndex = 0
+
+  const actionsCompiler: { [key: string]: () => void } = {
+    '+': () => counter++,
+    '-': () => counter--,
+    '*': () => (counter *= 2),
+    '%': () => (flagIndex = currentIndex),
+    '<': () => {
+      if (flagIndex === null) return
+      instructions[currentIndex] = ''
+      currentIndex = flagIndex
+      flagIndex = null
+    },
+    '¿': () => {
+      if (counter > 0) return
+      const endBracketIndex = instructions.indexOf('?')
+      instructions[endBracketIndex] = ''
+      currentIndex = endBracketIndex
+    },
+    '?': () => '',
+  }
+
+  for (currentIndex = 0; currentIndex < instructions.length; currentIndex++)
+    actionsCompiler[instructions[currentIndex]]?.()
+
+  return counter
 }
+
+compile('-¿+++?¿*+?')
