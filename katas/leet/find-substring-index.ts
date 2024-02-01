be018990b83d05a4c74719f0665626e0229d74ab
David Lezama <85713808+IamDavidev@users.noreply.github.com>
2024-02-01 14:15:36 +0000

    Added findSubstringIndex function to find the index of a substring in a string

--- katas/leet/find-substring-index.ts
+++ katas/leet/find-substring-index.ts
@@ -1,15 +1,20 @@
-
-
-
-
 export function findSubstringIndex(haystack: string, needle: string): number {
+  for (let index = 0; index < haystack.length; index++) {
+    const element = haystack[index]
 
-    console.table(haystack)
-    console.table(needle)
+    if (element !== needle[0]) continue
 
+    let isWord = true
 
-    return 0
+    for (let innerIndex = 1; innerIndex < needle.length; innerIndex++) {
+      if (haystack[index + innerIndex] !== needle[innerIndex]) isWord = false
+    }
+    if (isWord) return index
+  }
+
+  return -1
 }
 
-
-findSubstringIndex
+const str = 'sedbutsad'
+const needle = 'sad'
+console.log(findSubstringIndex(str, needle))
