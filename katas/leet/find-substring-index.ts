0814f7faafb8d3fd55e6cd6161718aaca79abbe2
David Lezama <85713808+IamDavidev@users.noreply.github.com>
2024-02-01 14:03:24 +0000

    Agregada función findSubstringIndex en el archivo find-substring-index.ts

---
+++ katas/leet/find-substring-index.ts
@@ -0,0 +1,15 @@
+
+
+
+
+export function findSubstringIndex(haystack: string, needle: string): number {
+
+    console.table(haystack)
+    console.table(needle)
+
+
+    return 0
+}
+
+
+findSubstringIndex
