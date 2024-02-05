3d2ca489997421633a44e4dd83ce0536da851252
David Lezama <85713808+IamDavidev@users.noreply.github.com>
2024-02-05 12:29:21 +0000

    add new kata  sequetial digits solution

---
+++ katas/leet/sequential-digits.ts
@@ -0,0 +1,19 @@
+export function sequentialDigits(low: number, high: number): number[] {
+  let result: number[] = []
+  const startDigits = '123456789'
+
+  for (let length = 2; length <= 9; length++) {
+    for (let start = 0; start <= 9 - length; start++) {
+      const numStr = startDigits.substring(start, start + length)
+      const num = parseInt(numStr, 10)
+      if (num >= low && num <= high) {
+        result.push(num)
+      }
+    }
+  }
+
+  console.log('Result', result)
+  return result
+}
+
+sequentialDigits(100, 300)
