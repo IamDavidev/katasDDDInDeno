3d85f6c3bc0d5d5bce288a0d3dca84dbaff7b8f4
David Lezama <85713808+IamDavidev@users.noreply.github.com>
2024-05-19 02:23:08 +0000

    refactor: Optimize areIsomorphic function

--- katas/leet/are-isomorphic.ts
+++ katas/leet/are-isomorphic.ts
@@ -2,27 +2,24 @@ export function areIsomorphic(s: string, t: string): boolean {
   if (s === '' && t === '') return true
   if (s.length !== t.length) return false
 
+  const assignedValues = new Set<string>()
   const replacedWords = new Map<string, string>()
-
-  let word = ''
 
   for (let i = 0; i < s.length; i++) {
     const letter = s[i]
     const toReplaceLetter = t[i]
 
     if (!replacedWords.has(letter)) {
-      if ([...replacedWords.values()].includes(toReplaceLetter)) return false
+      if (assignedValues.has(toReplaceLetter)) return false
+
       replacedWords.set(letter, toReplaceLetter)
+      assignedValues.add(toReplaceLetter)
     }
 
     const isValid = toReplaceLetter === replacedWords.get(letter)
 
     if (!isValid) return false
-
-    word += replacedWords.get(letter)
   }
-
-  if (word != t) return false
 
   return true
 }
