5112a67147a747edc2b147283d5673902f4bc06b
David Lezama <85713808+IamDavidev@users.noreply.github.com>
2024-09-04 03:28:44 +0000

    refactor: Merge two sorted arrays into one

---
+++ katas/interviews/1-merge.ts
@@ -0,0 +1,16 @@
+function merge(nums1: number[], m: number, nums2: number[], n: number): void {
+   let primaryIndex = m - 1;
+    let secondaryIndex = n - 1;
+    let mergedIndex = m + n - 1;
+
+    while (secondaryIndex >= 0) {
+        if (primaryIndex >= 0 && nums1[primaryIndex] > nums2[secondaryIndex]) {
+            nums1[mergedIndex] = nums1[primaryIndex];
+            primaryIndex--;
+        } else {
+            nums1[mergedIndex] = nums2[secondaryIndex];
+            secondaryIndex--;
+        }
+        mergedIndex--;
+    }
+}
