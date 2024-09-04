3f18546867a09ead9dd134ae2e5516345396941e
David Lezama <85713808+IamDavidev@users.noreply.github.com>
2024-09-04 03:28:48 +0000

    refactor: Optimize removeElement function

---
+++ katas/interviews/2-remove-element.ts
@@ -0,0 +1,12 @@
+function removeElement(nums: number[], val: number): number {
+  let nonValIndex = 0
+
+  for (let currentIndex = 0; currentIndex < nums.length; currentIndex++) {
+    if (nums[currentIndex] !== val) {
+      nums[nonValIndex] = nums[currentIndex]
+      nonValIndex++
+    }
+  }
+
+  return nonValIndex
+}
